5283639e2708dc91ea59d7e13b5ac80afab0842b
buco11 <aleksandarbucoivanovski@gmail.com>
2020-03-07 20:56:55 +0100

    Create Program.cs

---
+++ project2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = " dobar ";
+            string two = " den ";
+            var resOne = one + two;
+            Console.WriteLine(resOne);
+
+            string numOne = "9";
+            string numTwo = "3";
+            var resTwo = numOne + numTwo;
+
+            Console.WriteLine(resTwo);
+            Console.ReadLine();
+        }
+    }
+}
