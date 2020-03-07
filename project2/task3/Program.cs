a799846cb29e594d2317336ba821cdeb5086570a
buco11 <aleksandarbucoivanovski@gmail.com>
2020-03-07 20:59:05 +0100

    Create Program.cs

---
+++ project2/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int one = 102;
+            int two = 5;
+            var result = one / two;
+
+            Console.WriteLine("You can send: " +result +" messages");
+            Console.ReadLine();
+        }
+    }
+}
