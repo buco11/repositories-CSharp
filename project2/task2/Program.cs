df012456b0e6dec34254d92ef18258b7f4c34512
buco11 <aleksandarbucoivanovski@gmail.com>
2020-03-07 20:58:10 +0100

    Create Program.cs

---
+++ project2/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = "Alfa Romeo";
+            int two = 159;
+            var result = one + two;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
