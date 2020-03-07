6b70120d4dcdd60ebfe3cb5a3fddcb1ea3eb8acc
buco11 <aleksandarbucoivanovski@gmail.com>
2020-03-07 20:56:04 +0100

    Create Program.cs

---
+++ project2/bonusTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace bonusTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            int result;
+            bool res = int.TryParse(input, out result);
+
+            //Console.WriteLine(res);
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+            if (res)
+            {
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine("the number is even");
+                }
+                else
+				{
+					Console.WriteLine("the number is odd");
+				}
+			}
+			else
+			{
+				Console.WriteLine("enter a number");
+			}
+        }
+    }
+}
