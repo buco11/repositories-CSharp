067f434934e540bb1a9ab577c5a4d721dfe57836
buco11 <aleksandarbucoivanovski@gmail.com>
2020-04-01 14:59:44 +0200

    :)

---
+++ homework4/homework4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace homework4
+{
+    public class PhotoAlbum
+    {
+        private int NumberOfPages = 16;
+
+        public PhotoAlbum()
+        {
+                
+        }
+
+        public PhotoAlbum(int numberOfPages)
+        {
+            NumberOfPages = numberOfPages;
+        }
+
+        public void GetNumberOfPages()
+        {
+            Console.WriteLine($"the number of pages in the album is: {NumberOfPages}");
+        }
+    }
+
+    public class BigPhotoAlbum
+    {
+        private int NumberOfPages = 64;
+        public BigPhotoAlbum()
+        {
+            
+        }
+        public void GetNumberOfPages()
+        {
+            Console.WriteLine($"the number of pages in the album is: {NumberOfPages}");
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Task 1
+
+            //Create a class "PhotoAlbum" with a private attribute "numberOfPages".
+            //It should also have a public method "GetNumberOfPages",
+            //which will return the number of pages.
+            //The default constructor will create an album with 16 pages.
+            //There will be an additional constructor, with which we can specify the number of pages
+            //we want in the album.
+            //Create a class "BigPhotoAlbum" whose constructor will create an album with 64 pages.
+            //In main method create a "PhotoAlbum" instance with its default constructor and one with
+            //24 pages.
+            //Also create "BigPhotoAlbum" and show the number of pages that the three albums have.
+
+            var PhotoAlbum1 = new PhotoAlbum();
+            PhotoAlbum1.GetNumberOfPages();
+
+            var PhotoAlbum2 = new PhotoAlbum(24);
+            PhotoAlbum2.GetNumberOfPages();
+
+            var PhotoAlbum3 = new BigPhotoAlbum();
+            PhotoAlbum3.GetNumberOfPages();
+
+        }
+    }
+}
---
+++ homework4/task2/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace task2
+{
+    public class FirstCourse
+    {
+        public string Name;
+        public bool Pass;
+
+        public bool Passed()
+        {
+            return Pass;
+        }
+    }
+
+    public class SecondCourse
+    {
+        public string Name;
+        public int Grade;
+
+        public bool Passed()
+        {
+            if (Grade >= 6 && Grade <= 10) return true;
+            else return false;
+        }
+    }
+
+    public class Project
+    {
+        public FirstCourse course1;
+        public FirstCourse course2;
+        public SecondCourse course3;
+        public SecondCourse course4;
+
+        public bool Passed()
+        {
+            bool pass1 = course1.Passed();
+            bool pass2 = course2.Passed();
+            bool pass3 = course3.Passed();
+            bool pass4 = course4.Passed();
+
+            int counter = 0;
+
+            if (pass1) counter++;
+            if (pass2) counter++;
+            if (pass3) counter++;
+            if (pass4) counter++;
+
+            if (counter >= 3) return true;
+
+            else return false;
+
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Task 2
+
+            //In this exercise you are asked to program three simple classes which keep track
+            //of the grading of a sample student.
+            //The classes are called FirstCourse, SecondCourse, and Project.
+            //A FirstCourse encapsulates a course name and a registration of passed / not passed
+            //for our sample student.
+            //A SecondCourse encapsulates a course name and the grade of the student.
+            //For grading we use the grades,
+            //numerical grades 10, 9, 8, 7, 6, 5.You are also welcome use the enumeration.
+            //The grade 6 is the lowest passing grade.
+            //In both FirstCourse and SecondCourse you should write a method called Passed.
+            //The method is supposed to return whether our sample student passes the course.
+            //The class Project aggregates two FirstCourse courses and two SecondCourse courses.
+            //You can assume that a project is passed if at least three out
+            //of the four courses are passed.
+            //Write a method Passed in class Project which implements this passing policy.
+            //In Main method initialize 2 FirstCourse objects, 2 SecondCourse objects and 1 Project object.
+            //Add both FirstCourse and both SecondCourse objects like a properties to Project object.
+            //Call Project's object Passed method to see if the student passed.
+
+            FirstCourse firstCourse1 = new FirstCourse();
+            firstCourse1.Name = "C#";
+            firstCourse1.Pass = false;
+
+            FirstCourse firstCourse2 = new FirstCourse();
+            firstCourse2.Name = "JS";
+            firstCourse2.Pass = true;
+
+            SecondCourse secondCourse1 = new SecondCourse();
+            secondCourse1.Name = "Java";
+            secondCourse1.Grade = 6;
+
+            SecondCourse secondCourse2 = new SecondCourse();
+            secondCourse2.Name = "C++";
+            secondCourse2.Grade = 7;
+
+            Project project = new Project();
+            project.course1 = firstCourse1;
+            project.course2 = firstCourse2;
+            project.course3 = secondCourse1;
+            project.course4 = secondCourse2;
+
+            Console.WriteLine($"the student have passed C# {firstCourse1.Passed()}");
+            Console.WriteLine($"the student have passed JS {firstCourse2.Passed()}");
+
+            Console.WriteLine($"the student have passed Java {secondCourse1.Passed()}");
+            Console.WriteLine($"the student have passed C++ {secondCourse2.Passed()}");
+
+            bool result = project.Passed();
+            if (result) Console.WriteLine("The student have passed the project");
+            else Console.WriteLine("The student not passed");
+        }
+    }
+}
