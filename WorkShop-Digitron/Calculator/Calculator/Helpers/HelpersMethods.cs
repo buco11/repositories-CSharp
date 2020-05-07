5440b16525dc0e0e46abc6edb44602f6131338e9
buco11 <aleksandarbucoivanovski@gmail.com>
2020-05-07 08:30:58 +0200

    :)

---
+++ WorkShop-Digitron/Calculator/Calculator/Helpers/HelpersMethods.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Helpers
+{
+    internal static class HelpersMethods
+    {
+        internal static decimal CalculateAverage(List<int> numbers)
+        {
+            int sum = 0;
+            numbers.ForEach(number => sum += number);
+            return Convert.ToDecimal(sum) / Convert.ToDecimal(numbers.Count);
+        }
+
+        internal static int FindMaxNumber (List<int>numbers)
+        {
+            int max = 0;
+            foreach (var number in numbers)
+            {
+                if (number > max)
+                    max = number;
+            }
+            return max;
+        }
+
+        internal static int FindMinNumber(List<int>numbers)
+        {
+            int min = numbers[0];
+            foreach (var num in numbers)
+            {
+                if (num < min) min = num;
+            }
+            return min;
+        }
+
+        internal static int CalculateSum(List<int> numbers)
+        {
+            int sum = 0;
+            numbers.ForEach(number => sum += number);
+            return sum;
+        }
+
+        internal static int FindMinEvenNumber(List<int> numbers)
+        {
+            int minEven = numbers[0];
+            foreach (var num in numbers)
+            {                
+                if(num % 2 == 0 && num < minEven)
+                {
+                    minEven = num;
+                }               
+            }
+            return minEven;
+        }
+
+        internal static int FindMaxEvenNumber(List<int> numbers)
+        {
+            int maxEven = numbers[0];
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0 && num > maxEven)
+                {
+                    maxEven = num;
+                }
+            }
+            return maxEven;
+        }
+    }
+}
---
+++ WorkShop-Digitron/Calculator/Calculator/Models/Calc.cs
@@ -0,0 +1,39 @@
+﻿using Calculator.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Models
+{
+    public static class Calc
+    {
+        public static decimal Avg (List<int> numbers)
+        {
+            return HelpersMethods.CalculateAverage(numbers);
+        }
+        public static int Max (List<int> numbers)
+        {
+            return HelpersMethods.FindMaxNumber(numbers);
+        }
+
+        public static int Min(List<int> numbers)
+        {
+            return HelpersMethods.FindMinNumber(numbers);
+        }
+
+        public static int Sum(List<int> numbers)
+        {
+            return HelpersMethods.CalculateSum(numbers);
+        }
+
+        public static int MinEven(List<int> numbers)
+        {
+            return HelpersMethods.FindMinEvenNumber(numbers);
+        }
+
+        public static int MaxEven(List<int> numbers)
+        {
+            return HelpersMethods.FindMaxEvenNumber(numbers);
+        }
+    }
+}
